5bef53823fb92971edd5b2f4992a57ed1bd46fcb
Александр <sanya.kokorin.04@mail.ru>
2023-09-09 00:37:39 +1000

    Добавьте файлы проекта.

---
+++ StarterResurs/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Diagnostics;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Не вздумай закрывать эту консоль!!!");
+
+    while (true) //бесконечный цикл
+    {
+        var processes = Process.GetProcesses(); //Получаем все процессы
+        var only = processes.FirstOrDefault(s => s.ProcessName == "notepad"); //Ищем среди всех процессов нужный
+
+            if (only == null) //Делаем проверку на ненайденный нужный процесс
+            {
+                using Process process = new Process();
+                {
+                    process.StartInfo.FileName = "notepad";
+                    //process.StartInfo.FileName = @"C:\Program Files\UrbanVPN\bin\urbanvpn-gui.exe"; //путь к приложению, которое нужно запустить
+                    process.Start(); //Запускаем нужный процесс
+                };
+            }
+
+        Thread.Sleep(300000); //Мониторинг каждые 5 минут
+    }
+
