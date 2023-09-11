00044fc0c819b6f2c5435ef217513be2faca2e5d
sigur <sigur@DESKTOP-BQ5P2JF>
2023-09-11 12:22:54 +1000

    f

--- StarterResurs/Program.cs
+++ StarterResurs/Program.cs
@@ -2,24 +2,38 @@
 using System;
 using System.Diagnostics;
 
-Console.ForegroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("Не вздумай закрывать эту консоль!!!");
 
     while (true) //бесконечный цикл
     {
-        var processes = Process.GetProcesses(); //Получаем все процессы
-        var only = processes.FirstOrDefault(s => s.ProcessName == "notepad"); //Ищем среди всех процессов нужный
+        try
+        {
+            var processes = Process.GetProcesses(); //Получаем все процессы
+            var only = processes.FirstOrDefault(s => s.ProcessName == "Resurs.exe"); //Ищем среди всех процессов нужный
 
             if (only == null) //Делаем проверку на ненайденный нужный процесс
             {
                 using Process process = new Process();
                 {
-                    process.StartInfo.FileName = "notepad";
-                    //process.StartInfo.FileName = @"C:\Program Files\UrbanVPN\bin\urbanvpn-gui.exe"; //путь к приложению, которое нужно запустить
+                    process.StartInfo.FileName = @"C:\Program Files(x86)\АРМ Ресурс XE\Resurs.exe"; //путь к приложению, которое нужно запустить
                     process.Start(); //Запускаем нужный процесс
                 };
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{DateTime.Now} - Открываем \"Ресурс\"");
+        }
+            else
+            {
+                Console.WriteLine($"{DateTime.Now} - \"Ресурс\" оказался открытым");
             }
 
-        Thread.Sleep(300000); //Мониторинг каждые 5 минут
+            Thread.Sleep(300000); //Мониторинг каждые 5 минут
+        }
+        catch
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Непредвиденная ошибка!");
+        }
+        
     }
 
