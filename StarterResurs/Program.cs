c8bf5fff0bc79d01c68c84674efe13f0f344d7a5
Александр Кокорин <sanya.kokorin.04@mail.ru>
2023-10-31 08:32:15 +1000

    gg

--- StarterResurs/Program.cs
+++ StarterResurs/Program.cs
@@ -5,35 +5,49 @@ using System.Diagnostics;
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("Не вздумай закрывать эту консоль!!!");
 
-    while (true) //бесконечный цикл
+while (true)
+{
+    try
     {
-        try
-        {
-            var processes = Process.GetProcesses(); //Получаем все процессы
-            var only = processes.FirstOrDefault(s => s.ProcessName == "Resurs.exe"); //Ищем среди всех процессов нужный
+        var processes = Process.GetProcesses(); //Получаем все процессы
+        var runningStarterResurs = Process.GetProcesses().Where(s => s.ProcessName == "StarterResurs").Count();
 
-            if (only == null) //Делаем проверку на ненайденный нужный процесс
+        if (runningStarterResurs >= 2)
+        {
+            var pr = Process.GetCurrentProcess();
+            pr.Kill();
+        }
+        else
+        {
+            var resurs = processes.FirstOrDefault(s => s.ProcessName == "notepad"); //Ищем среди всех процессов нужный
+
+            if (resurs == null) //Делаем проверку на ненайденный нужный процесс
             {
                 using Process process = new Process();
                 {
-                    process.StartInfo.FileName = @"C:\Program Files(x86)\АРМ Ресурс XE\Resurs.exe"; //путь к приложению, которое нужно запустить
+                    process.StartInfo.FileName = "Notepad"; //путь к приложению, которое нужно запустить
                     process.Start(); //Запускаем нужный процесс
                 };
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"{DateTime.Now} - Открываем \"Ресурс\"");
-        }
+            }
             else
             {
                 Console.WriteLine($"{DateTime.Now} - \"Ресурс\" оказался открытым");
             }
 
             Thread.Sleep(300000); //Мониторинг каждые 5 минут
+
+            
         }
-        catch
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Непредвиденная ошибка!");
-        }
-        
+
     }
+    catch
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Непредвиденная ошибка!");
+    }
+}
+        
+    
 
